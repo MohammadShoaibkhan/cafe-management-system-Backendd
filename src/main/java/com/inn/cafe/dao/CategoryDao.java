b9b0486327c0171b69a098bb971042493ccb70df
MohammadShoaibkhan <2000shoaibkhan@gmail.com>
2024-07-02 22:04:14 +0530

    first commit

---
+++ src/main/java/com/inn/cafe/dao/CategoryDao.java
@@ -0,0 +1,13 @@
+package com.inn.cafe.dao;
+
+import com.inn.cafe.POJO.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
+public interface CategoryDao extends JpaRepository<Category, Integer> {
+
+    List<Category> getAllCategory();
+
+}
